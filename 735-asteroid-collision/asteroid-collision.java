4fa425856c1952e5dbc24de517c0e21d0948b367
1amanali2468 <96293452+1amanali2468@users.noreply.github.com>
2026-06-26 00:59:28 +0530

    Time: 4 ms (82.94%) | Memory: 46.7 MB (82.51%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean destroyed = false;
+
+            while (!st.isEmpty() && st.peek() > 0 && a < 0) {
+                int top = st.peek();
+
+                if (top < -a) {
+                    st.pop(); // top explodes
+                } 
+                else if (top == -a) {
+                    st.pop(); // both explode
+                    destroyed = true;
+                    break;
+                } 
+                else {
+                    destroyed = true; // current explodes
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                st.push(a);
+            }
+        }
+
+        int[] res = new int[st.size()];
+        for (int i = st.size() - 1; i >= 0; i--) {
+            res[i] = st.pop();
+        }
+
+        return res;
+    }
+}
