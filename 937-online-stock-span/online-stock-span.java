345f32eeacd7d863d9a711ede918809809c5829e
1amanali2468 <96293452+1amanali2468@users.noreply.github.com>
2026-06-25 18:40:18 +0530

    Time: 29 ms (85.45%) | Memory: 54.2 MB (72.24%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class StockSpanner {
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+
+        // merge spans of smaller/equal prices
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span += st.pop()[1];
+        }
+
+        st.push(new int[]{price, span});
+        return span;
+    }
+}
