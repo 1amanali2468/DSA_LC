b3f183696554e98674d63d93a1dc129f10c5b552
1amanali2468 <96293452+1amanali2468@users.noreply.github.com>
2026-07-02 14:49:07 +0530

    Time: 2 ms (6.18%) | Memory: 48.5 MB (28.39%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,43 +1,34 @@
+import java.util.*;
 class Solution {
-     public int trap(int[] height) {
+    public static int[] prefixMax(int[] height){
+        int[] prefix = new int[height.length];
+        prefix[0] = height[0];
+        for(int i=1; i<prefix.length; i++){
+            prefix[i] = Math.max(prefix[i-1], height[i]);
+        }
+        return prefix;
+    }
+    public static int[] suffixMax(int[] height){
         int n = height.length;
-        
-        // Initialize two pointers at both ends of the array
-        int left = 0;
-        int right = n - 1;
-        
-        // Variables to track the maximum height to the left and right
-        int maxLeft = 0;
-        int maxRight = 0;
-        
-        // Variable to store total trapped water
-        int totalWater = 0;
-        
-        // Iterate until left pointer meets right pointer
-        while (left <= right) {
-            // If left bar is smaller or equal to right bar
-            if (height[left] <= height[right]) {
-                // If current left bar is higher than maxLeft, update maxLeft
-                if (height[left] >= maxLeft) {
-                    maxLeft = height[left];
-                } else {
-                    // Water trapped on left is difference between maxLeft and current height
-                    totalWater += maxLeft - height[left];
-                }
-                left++; // Move left pointer to the right
-            } else {
-                // If current right bar is higher than maxRight, update maxRight
-                if (height[right] >= maxRight) {
-                    maxRight = height[right];
-                } else {
-                    // Water trapped on right is difference between maxRight and current height
-                    totalWater += maxRight - height[right];
-                }
-                right--; // Move right pointer to the left
+        int[] suffix = new int[n];
+        suffix[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffix[i] = Math.max(suffix[i+1], height[i]);
+        }
+        return suffix;
+    }
+    public int trap(int[] height) {
+        int n = height.length;
+        int total =0;
+        int[] prefixMax = prefixMax(height);
+        int[] suffixMax = suffixMax(height);
+        for(int i=0; i<n; i++){
+            int leftmax = prefixMax[i];
+            int rightmax  = suffixMax[i];
+            if(height[i]<leftmax && height[i]<rightmax){
+                total += Math.min(leftmax, rightmax) - height[i];
             }
         }
-        
-        // Return total trapped water
-        return totalWater;
+        return total;
     }
 }
