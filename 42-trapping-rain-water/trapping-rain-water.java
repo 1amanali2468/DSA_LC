acdcfec894ccc27a5544eb42f04deaa5d48ebc90
1amanali2468 <96293452+1amanali2468@users.noreply.github.com>
2026-07-02 15:03:00 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (38.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,27 @@
 import java.util.*;
 class Solution {
-    public static int[] prefixMax(int[] height){
-        int[] prefix = new int[height.length];
-        prefix[0] = height[0];
-        for(int i=1; i<prefix.length; i++){
-            prefix[i] = Math.max(prefix[i-1], height[i]);
-        }
-        return prefix;
-    }
-    public static int[] suffixMax(int[] height){
-        int n = height.length;
-        int[] suffix = new int[n];
-        suffix[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--){
-            suffix[i] = Math.max(suffix[i+1], height[i]);
-        }
-        return suffix;
-    }
     public int trap(int[] height) {
         int n = height.length;
-        int total =0;
-        int[] prefixMax = prefixMax(height);
-        int[] suffixMax = suffixMax(height);
-        for(int i=0; i<n; i++){
-            int leftmax = prefixMax[i];
-            int rightmax  = suffixMax[i];
-            if(height[i]<leftmax && height[i]<rightmax){
-                total += Math.min(leftmax, rightmax) - height[i];
+        int leftMax =0,  rightMax=0, total =0;
+        int l=0, r=n-1;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(leftMax>height[l]){
+                    total += leftMax - height[l];
+                }
+                else{
+                    leftMax = height[l];
+                }
+                l++;
+            }
+            else{
+                if(rightMax > height[r]){
+                    total += rightMax - height[r];
+                }
+                else{
+                    rightMax = height[r];
+                }
+                r--;
             }
         }
         return total;
