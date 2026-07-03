91fb82178bbf3dae5e4b80ee26f1382ca1407ad4
1amanali2468 <96293452+1amanali2468@users.noreply.github.com>
2026-07-03 13:13:38 +0530

    Time: 61 ms (76.96%) | Memory: 78.4 MB (30.28%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,50 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nse = findNSE(heights);
-        int[] pse = findPSE(heights);
-
-        int maxi = 0;
-
-        for (int i = 0; i < heights.length; i++) {
-            int width = nse[i] - pse[i] - 1;
-            int area = heights[i] * width;
-            maxi = Math.max(maxi, area);
-        }
-
-        return maxi;
-    }
-
-    private int[] findNSE(int[] arr) {
-        int n = arr.length;
-        int[] nse = new int[n];
         Stack<Integer> st = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
+        int maxArea=0;
+        int n = heights.length;
+        int ind,nse,pse;
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
+                ind = st.pop();
+                nse = i;
+                pse = st.isEmpty() ? -1 : st.peek();
+                maxArea = Math.max(maxArea, heights[ind]*(nse-pse-1));
             }
-            nse[i] = st.isEmpty() ? n : st.peek();
             st.push(i);
         }
-
-        return nse;
-    }
-
-    private int[] findPSE(int[] arr) {
-        int n = arr.length;
-        int[] pse = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
-            }
-            pse[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
+        while(!st.isEmpty()){
+            nse = n;
+            ind = st.pop();
+            pse = st.isEmpty() ? -1 : st.peek();
+            maxArea = Math.max(maxArea, heights[ind]*(nse-pse-1));
         }
-
-        return pse;
+        return maxArea;
     }
 }
