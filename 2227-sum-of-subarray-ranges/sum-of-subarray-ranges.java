51f0cd53a64a01910b96e8a00526e8cf1ff2a592
1amanali2468 <96293452+1amanali2468@users.noreply.github.com>
2026-07-03 11:22:56 +0530

    Time: 26 ms (44.69%) | Memory: 46.4 MB (65.07%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,86 +1,15 @@
-import java.util.*;
-
 class Solution {
     public long subArrayRanges(int[] nums) {
-        int n = nums.length;
-
-        long minSum = getMinSum(nums);
-        long maxSum = getMaxSum(nums);
-
-        return maxSum - minSum;
-    }
-
-    private long getMinSum(int[] arr) {
-        int n = arr.length;
-        int[] nse = new int[n];
-        int[] pse = new int[n];
-
-        Stack<Integer> st = new Stack<>();
-
-        // NSE (next smaller or equal)
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
-            }
-            nse[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-
-        st.clear();
-
-        // PSE (previous smaller)
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
-                st.pop();
-            }
-            pse[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
-
         long sum = 0;
-        for (int i = 0; i < n; i++) {
-            long left = i - pse[i];
-            long right = nse[i] - i;
-            sum += left * right * arr[i];
-        }
-
-        return sum;
-    }
-
-    private long getMaxSum(int[] arr) {
-        int n = arr.length;
-        int[] nge = new int[n];
-        int[] pge = new int[n];
-
-        Stack<Integer> st = new Stack<>();
-
-        // NGE (next greater or equal)
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
-                st.pop();
+        for(int i=0; i<nums.length; i++){
+            long largest = nums[i];
+            long smallest = nums[i];
+            for(int j=i+1; j<nums.length; j++){
+                largest = Math.max(largest, nums[j]);
+                smallest = Math.min(smallest, nums[j]);
+                sum += (largest-smallest);
             }
-            nge[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
         }
-
-        st.clear();
-
-        // PGE (previous greater)
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
-                st.pop();
-            }
-            pge[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
-
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            long left = i - pge[i];
-            long right = nge[i] - i;
-            sum += left * right * arr[i];
-        }
-
         return sum;
     }
 }
